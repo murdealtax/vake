7505a1f767d4bc2e13bd2a3f558b64c82486b02a
syntax <landonpotter@gmail.com>
2025-06-19 09:52:06 -0500

    [Check] Remove entry_file check for now

--- src/parser/check.rs
+++ src/parser/check.rs
@@ -13,20 +13,30 @@ pub fn init(recipe: &Recipe) {
     let mut recipe_errors = 0;
 
     if !active_directory.exists() {
-        error!("The active directory '{:?}' does not exist", options.active_directory.as_os_str().to_str().expect("Expected a specified directory"));
+        error!(
+            "The active directory '{:?}' does not exist",
+            options
+                .active_directory
+                .as_os_str()
+                .to_str()
+                .expect("Expected a specified directory")
+        );
         recipe_errors += 1;
     }
 
-    if !entry_file.exists() {
-        error!("The specified entry file '{}' does not exist", entry_file.display());
-        recipe_errors += 1;
-    }
+    // if !entry_file.exists() {
+    //     error!("The specified entry file '{}' does not exist", entry_file.display());
+    //     recipe_errors += 1;
+    // }
 
     for entry in entries {
         let path = active_directory.join(entry.path.clone());
 
         if !path.exists() {
-            error!("The directory specified for recipe '{}' does not exist", path.display());
+            error!(
+                "The directory specified for recipe '{}' does not exist",
+                path.display()
+            );
             recipe_errors += 1;
         }
     }
@@ -35,13 +45,24 @@ pub fn init(recipe: &Recipe) {
         let path = active_directory.join(association.path.clone());
 
         if !path.exists() {
-            error!("The directory specified for association '{}' does not exist", path.display());
+            error!(
+                "The directory specified for association '{}' does not exist",
+                path.display()
+            );
             recipe_errors += 1;
         }
     }
 
     if recipe_errors > 0 {
-        error!("Recipe failed with {} error(s) when attempting to build from directory '{}'", recipe_errors, options.active_directory.as_os_str().to_str().expect("Expected a specified directory"));
+        error!(
+            "Recipe failed with {} error(s) when attempting to build from directory '{}'",
+            recipe_errors,
+            options
+                .active_directory
+                .as_os_str()
+                .to_str()
+                .expect("Expected a specified directory")
+        );
         std::process::exit(1);
     }
 }
